9d5a4bbfddf4e9a6e3473a0dbacf346d8ede5205
chackochii <edwinchacko2001@gmail.com>
2025-11-11 22:59:54 +0530

    vite admin break issue fix

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5174}
+  server: {
+    port: 5174,
+    host: true,        // required for EC2 dev access
+    strictPort: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  }
 })
